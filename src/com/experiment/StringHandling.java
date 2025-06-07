a162fb4982071d94a2991eaa5543716dc771789e
Notu <multipurpose95sourav@gmail.com>
2025-06-07 17:19:15 +0530

    good commit

---
+++ src/com/experiment/StringHandling.java
@@ -0,0 +1,12 @@
+package com.experiment;
+
+public class StringHandling {
+
+    public static void main(String[] args) {
+        System.out.println(10 * 20 + "sample");
+        System.out.println("test" + 10 * 20);
+
+        System.out.println(10 + 20 + "sample");
+        System.out.println("test" + 10 + 20);
+    }
+}
